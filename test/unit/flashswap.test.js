232be5bacedb23bf815ec8ae8df494281052080c
Legazole <LegazoleNFT@gmail.com>
2023-02-06 19:14:55 +0100

    started flashswap gradual testing

---
+++ test/unit/flashswap.test.js
@@ -0,0 +1,13 @@
+const { ethers, getNamedAccounts } = require("hardhat")
+const { expect, assert } = require("chai")
+const { deployments } = require("hardhat")
+
+describe("Uniswap flashswap functionality", function () {
+    let flashswapInterface,
+        uniswapInterface,
+        swapInterface,
+        bornCoin,
+        geneCoin,
+        deployer
+    beforeEach(async function () {})
+})
